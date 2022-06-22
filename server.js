210e5d1e888b9a796202ededde5c9f1eadcf45aa
Noah Hoffman <noah8863@Noahs-MacBook-Air.local>
2022-06-22 17:11:23 -0600

    created first route to view all users

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
+const { User } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,7 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set up get routes for user here
-
+app.get('/api/users', (req, res) => {
+    User.find({}, (err, result) => {
+        if (err) {
+            res.status(500).send({message: err.message});
+        } else {
+            res.status(200).json(result);
+        }
+    })
+})
 
 db.once('open', function() {
     app.listen(PORT, () => {
