74a8b277ab8892da8096cc5d8c4db764107ff0d3
Noah8863 <stacknoah99@gmail.com>
2022-06-22 18:54:41 -0600

    creating routes

--- models/User.js
+++ models/User.js
@@ -6,6 +6,13 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email must be valid!']},
     thoughts: [{type: ObjectId, ref: "Thought"}],
     friends: [{type: ObjectId, ref: "User"}]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+    //Dont assign an ID to the virtuals
 })
 
 userSchema.virtual('friendCount').get(function (){
